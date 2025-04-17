d34ded5071f8622da270501553ca2f3abb5890e3
Manolo Bassot <manolo.bassot97@gmail.com>
2025-04-16 21:10:41 -0300

    Leccion 2 significado letras

--- Python/Leccion02/manejo_archivos.py
+++ Python/Leccion02/manejo_archivos.py
@@ -5,6 +5,8 @@ try:
     archivo.write('Programamos con diferentes tipos de archvos, ahora en txt.\n')
     archivo.write('Los acentos son importantes para las palabras\n')
     archivo.write('como por ejemplo: acción, ejecucion, producción  \n')
+    archivo.write('las letras son:\nr read leer, \na append agregar, \nw write escribir, \nx crea un archivo')
+    archivo.write('\nt esta es para texto, \nb para binario, \nw+ lee y escribe son iguales r+\n')
     archivo.write('Con esto terminamos.\n')
 except Exception as e:
     print(e)
