8fe2b5b608c325e207e9f14005ad352bf0521bc2
Franco Moyano <franco.moyano.sr@gmail.com>
2025-06-23 23:49:23 -0300

    fix de ejecución para todos los ambientes

--- Proyecto-Integrador-Python/Space-Invaders-UTN/src/main.py
+++ Proyecto-Integrador-Python/Space-Invaders-UTN/src/main.py
@@ -1,3 +1,8 @@
+import sys
+import os
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 import pygame
 from settings import *           # Importa constantes de configuración (pantalla, FPS, etc.)
 from core.game import Game      # Importa la clase principal del juego
